f324ce95ddb5b9da72234b6cba92edaa4bacaee7
iamptic <65834099+iamptic@users.noreply.github.com>
2025-08-14 20:33:33 +0300

    Add files via upload

--- web/server.js
+++ web/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
 const app = express();
 const WEB_DIR = path.join(__dirname, 'web');
 const FOODY_API = process.env.FOODY_API || '';
